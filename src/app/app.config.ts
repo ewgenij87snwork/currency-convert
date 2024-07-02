5827d7f38c274028b64e434c6f3009fc51c59de7
ewgenij87snwork <ewgenij87snwork@gmail.com>
2024-07-03 00:56:07 +0300

    ADD | Angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [importProvidersFrom(HttpClientModule)]
+  providers: [importProvidersFrom(HttpClientModule), provideAnimationsAsync()]
 };
